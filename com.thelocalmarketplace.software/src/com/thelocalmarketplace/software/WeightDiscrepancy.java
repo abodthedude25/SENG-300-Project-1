0a81671c6b50acb4040aedc54d11e3b49caf7ce8
emily-gv <emilygarciavolk@gmail.com>
2024-03-08 13:45:11 -0700

    Added initial version of checkRemoval method

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/WeightDiscrepancy.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/WeightDiscrepancy.java
@@ -1,5 +1,39 @@
 package com.thelocalmarketplace.software;
 
-public class WeightDiscrepancy {
+import java.math.BigDecimal;
 
+public class WeightDiscrepancy {
+	
+	private Order order;
+	private BigDecimal weightAtBlock;
+	
+    /**
+     * Records weight at time of discrepancy before block
+     * 
+     */
+	private void getWeightAtBlock() {
+		weightAtBlock = order.getWeight();
+	}
+	
+	/**
+	 * Constructor for order
+	 */
+	public WeightDiscrepancy(Order order) {
+		this.order = order;
+		getWeightAtBlock();
+	}
+	
+    /**
+     * Compares weight at block to current getWeight to check if an item has been removed.
+     * 
+     * @return True if item has been removed (new weight is less). False otherwise.
+     */
+	public boolean checkRemoval() {
+		BigDecimal currentWeight = order.getWeight();
+		if (currentWeight.compareTo(weightAtBlock) < 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
