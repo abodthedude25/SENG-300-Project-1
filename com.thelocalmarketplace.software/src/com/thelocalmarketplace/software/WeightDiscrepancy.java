cbdb477f1674edb6a58b7bcc479a5996faca3cf2
Izabella <Izabella.mawani@gmail.com>
2024-03-08 22:52:02 -0700

    added updatemass and setblocked

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/WeightDiscrepancy.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/WeightDiscrepancy.java
@@ -1,9 +1,14 @@
 package com.thelocalmarketplace.software;
 
+import java.awt.event.ItemEvent;
 import java.math.BigDecimal;
+import java.util.List; 
 
 public class WeightDiscrepancy {
 	
+	private List<Item> items;
+	private ElectronicScale scale;
+	private boolean blocked;
 	private Order order;
 	private BigDecimal weightAtBlock;
 	
@@ -18,9 +23,46 @@ public class WeightDiscrepancy {
 	/**
 	 * Constructor for order
 	 */
-	public WeightDiscrepancy(Order order) {
+	public WeightDiscrepancy(Order order, ElectronicScale scale) {
 		this.order = order;
+		this.items = order.getOrder();
+		this.scale = (ElectronicScale) scale;
 		getWeightAtBlock();
+	}
+	
+	/**
+	 * Updates the scale mass every time an item is added to or removed from the order
+	 */
+	/**
+	 * Records weight at time of discrepancy before block 
+	 */
+	public void updateMass() {
+		try {
+			Mass currMass = scale.getCurrentMassOnTheScale();
+			currMass = Mass.ZERO;
+			for (Item item : items) {
+				scale.addAnItem(item);
+			}
+		} catch (OverloadedDevice e) {
+			blocked = true;
+		}
+	}
+	
+	public void setBlocked() {
+		Mass actual;
+		BigDecimal expected;
+		try {
+			actual = scale.getCurrentMassOnTheScale();
+			expected = order.getWeight();
+			
+			blocked = !expected.equals(actual);
+		} catch (OverloadedDevice e) {
+			blocked = true;
+		}
+	}
+	
+	public boolean isBlocked() {
+		return blocked;
 	}
 	
     /**
