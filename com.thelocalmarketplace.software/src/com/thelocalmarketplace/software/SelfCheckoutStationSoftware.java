ba6eb23383f545754f2b016c9df82e6eebcaaa9a
Talaal Irtija <irtijat@gmail.com>
2024-03-09 17:47:16 -0700

    added documentation to functions/variables

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutStationSoftware.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutStationSoftware.java
@@ -1,16 +1,32 @@
-package com.thelocalmarketplace.software;
+/** Talaal Irtija (UCID: 30169780)
+ * Yotam Rojnov (UCID: 30173949)*/
 
+
+
+package com.thelocalmarketplace.software;
 public class SelfCheckoutStationSoftware {
+
+	/** Boolean variable that is used to track whether user interaction is blocked */
 	private boolean blocked = false;
-	
+
+	/** Boolean variable to track if a current session is active or not. */
+	private boolean active;
+
+	/** Boolean variable to track whether the self checkout station is powered on. */
+	private boolean enabled;
+
+	/** Set function to change the blocked variable value
+	 * @param value The new value for station block status*/
 	public void setStationBlock(boolean value) {
 		this.blocked = value;
 	}
-	
+
+	/** Get function to get the blocked station status*/
 	public boolean getStationBlock() {
 		return this.blocked;
 	}
 
+	/** Function to start a session for self checkout machine*/
 	public void startSession() {
 		if () {
 
