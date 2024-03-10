4ed1fd6af5da6c7a853b24820da0e7eccacb8cb7
Joseph Tandyo <josephtandyo@gmail.com>
2024-03-09 17:40:19 -0700

    test commit

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Order.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Order.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import com.jjjwelectronics.Item;
 import com.jjjwelectronics.scanner.BarcodedItem;
 
-// hello secret message
 /**
  * Represents the Customer's order that different use cases can interact with.
  */
