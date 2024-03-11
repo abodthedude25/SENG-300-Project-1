3642ef73ef9e144a9ffab07dd7ed21e547e562f9
Talaal Irtija <irtijat@gmail.com>
2024-03-10 23:02:24 -0600

    small changes to code

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutStationSoftware.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/SelfCheckoutStationSoftware.java
@@ -1,8 +1,8 @@
-/** Talaal Irtija (UCID: 30169780)
+/**
+ * Talaal Irtija (UCID: 30169780)
  * Yotam Rojnov (UCID: 30173949)
- * Joseph Tandyo (UCID: 30182561) */
-
-
+ * Joseph Tandyo (UCID: 30182561)
+ */
 
 package com.thelocalmarketplace.software;
 
@@ -11,27 +11,32 @@ import com.thelocalmarketplace.hardware.SelfCheckoutStation;
 
 public class SelfCheckoutStationSoftware {
 
-	/** Boolean variable that is used to track whether user interaction is blocked */
+	/**
+	 * Boolean variable that is used to track whether user interaction is blocked
+	 */
 	private boolean blocked = false;
 
 	/** Boolean variable to track if a current session is active or not. */
 	private boolean active;
 
-	/** Boolean variable to track whether the self checkout station is powered on. */
-	private boolean enabled;
-
-	/** Set function to change the blocked variable value
-	 * @param value The new value for station block status*/
+	/**
+	 * Set function to change the blocked variable value
+	 * @param value The new value for station block status
+	 */
 	public void setStationBlock(boolean value) {
 		this.blocked = value;
 	}
 
-	/** Get function to get the blocked station status*/
+	/**
+	 * Get function to get the blocked station status
+	 */
 	public boolean getStationBlock() {
 		return this.blocked;
 	}
 
-	/** Function to start a session for self-checkout machine*/
+	/**
+	 * Function to start a session for self-checkout machine
+	 */
 	public void startSession() {
 
 		// Check if there is a current active session.
