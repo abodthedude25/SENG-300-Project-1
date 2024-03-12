6d0deefa19e85bb399d1e6668013951d81085a06
Lilia Skumatova <148629607+LiliaSkumatova@users.noreply.github.com>
2024-03-12 01:12:36 -0600

    Update PaymentHandler.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PaymentHandler.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PaymentHandler.java
@@ -2,6 +2,7 @@
  * Duncan McKay (UCID: 30177857)
  * Mahfuz Alam (UCID:30142265)
  * Luis Trigueros Granillo (UCID: 30167989)
+ * Lilia Skumatova (UCID: 30187339)
  */
 
 package com.thelocalmarketplace.software;
