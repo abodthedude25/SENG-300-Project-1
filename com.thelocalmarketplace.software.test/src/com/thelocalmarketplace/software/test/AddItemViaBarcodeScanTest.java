a037ae2f905934660a2428c3e0c3ac4541e97500
yuinikoru.futamata <yuinikoru.futamata@ucalgary.ca>
2024-03-12 21:19:58 -0600

    Added testDetectsBarcode()

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddItemViaBarcodeScanTest.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/AddItemViaBarcodeScanTest.java
@@ -33,16 +33,16 @@ public class AddItemViaBarcodeScanTest {
 	AddItemViaBarcodeScan testBarcodeItemAdder;
 	Order testOrder;
 	BaggingAreaListener testBaggingAreaListener;
-	
+
 	@Before
 	public void setUp() {
 		// Make a power grid for hardware to connect to
 		grid = PowerGrid.instance();
-		
+
 		// Initializing hardware objects
 		scanner = new BarcodeScanner();
 		baggingArea = new ElectronicScale();
-		
+
 		// Power up and enable hardware objects
 		scanner.plugIn(grid);
 		scanner.turnOn();
@@ -50,51 +50,52 @@ public class AddItemViaBarcodeScanTest {
 		baggingArea.plugIn(grid);
 		baggingArea.turnOn();
 		baggingArea.enable();
-		
+
 		// Initializing mock barcoded item
-        Numeral[] barcodeDigits = {Numeral.one, Numeral.two, Numeral.three, Numeral.four, Numeral.five};
-        Barcode barcode = new Barcode(barcodeDigits);
+		Numeral[] barcodeDigits = {Numeral.one, Numeral.two, Numeral.three, Numeral.four, Numeral.five};
+		Barcode barcode = new Barcode(barcodeDigits);
 		Mass itemMass = new Mass(1000000000); // 1kg in micrograms
 		barcodedItem = new BarcodedItem(barcode, itemMass);
-		
+
 		// Initializing mock product (using same barcode as the barcoded item)
 		String productDescription = "test product";
 		long productPrice = 5;
 		double productWeightInGrams = 1000;
 		barcodedProduct = new BarcodedProduct(barcode, productDescription, productPrice, productWeightInGrams);
-		
+
 		// Adding mock product into product database
 		ProductDatabases.BARCODED_PRODUCT_DATABASE.put(barcode, barcodedProduct);
-		
+
 		// Initializing testOrder
 		testOrder = new Order();
-		
+
 		// Initializing testBarcodeItemAdder and making it listen to the scanner object
 		testBarcodeItemAdder = new AddItemViaBarcodeScan(testOrder);
 		scanner.register(testBarcodeItemAdder);
-		
+
 		// Initializing testBaggingAreaListener and making it listen to the scale object
 		testBaggingAreaListener = new BaggingAreaListener();
 		baggingArea.register(testBaggingAreaListener);
 	}
-	
+
 	@Test
-    public void testAddingItem() {
-        baggingArea.addAnItem(barcodedItem);
-    }
-	
+	public void testDetectsBarcode() {
+		// work in progress
+		scanner.scan(barcodedItem);
+	}
+
 	@After
-    public void tearDown() {
-        // de-register listeners 
-        scanner.deregister(testBarcodeItemAdder);
-        baggingArea.deregister(testBaggingAreaListener);
-        
-        // Disable, turn off and un-plug hardware objects
-        scanner.disable();
-        scanner.turnOff();
-        scanner.unplug();
-        baggingArea.disable();
-        baggingArea.turnOff();
-        baggingArea.unplug();
-    }
+	public void tearDown() {
+		// de-register listeners 
+		scanner.deregister(testBarcodeItemAdder);
+		baggingArea.deregister(testBaggingAreaListener);
+
+		// Disable, turn off and un-plug hardware objects
+		scanner.disable();
+		scanner.turnOff();
+		scanner.unplug();
+		baggingArea.disable();
+		baggingArea.turnOff();
+		baggingArea.unplug();
+	}
 }
