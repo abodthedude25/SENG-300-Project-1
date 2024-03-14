372d38437d2a38831ef089c2018ff3cacc267f95
Abil <135502232+Astroboy1011@users.noreply.github.com>
2024-03-14 13:27:19 -0600

    Create Demo.java

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Demo.java
@@ -0,0 +1,15 @@
+/*
+ * Displays a demo version of the self checkout system
+ * 
+ */
+
+package com.thelocalmarketplace.software;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
