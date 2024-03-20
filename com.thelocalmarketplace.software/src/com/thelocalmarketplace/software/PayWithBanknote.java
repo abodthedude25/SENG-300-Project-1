6635422980b72f3c234cecf2116d2d6c417f648b
Lilia Skumatova <148629607+LiliaSkumatova@users.noreply.github.com>
2024-03-20 17:02:40 -0600

    Update PayWithBanknote.java

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithBanknote.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayWithBanknote.java
@@ -1,167 +1,138 @@
 package com.thelocalmarketplace.software;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Currency;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Formatter.BigDecimalLayoutForm;
-
 import com.tdc.CashOverloadException;
 import com.tdc.DisabledException;
-import com.tdc.IComponent;
-import com.tdc.IComponentObserver;
 import com.tdc.NoCashAvailableException;
 import com.tdc.banknote.Banknote;
-import com.tdc.banknote.BanknoteDispenserBronze;
 import com.tdc.banknote.BanknoteInsertionSlot;
-import com.tdc.banknote.BanknoteStorageUnit;
 import com.tdc.banknote.BanknoteValidator;
-import com.tdc.banknote.BanknoteValidatorObserver;
+
 import com.thelocalmarketplace.hardware.SelfCheckoutStationBronze;
 
-// How do we use self-checkout station gold?
-// The total amount get passed though as a parameter
+//figure out how to use gold?
 
-public class PayWithBanknote implements BanknoteValidatorObserver {
+import com.jjjwelectronics.EmptyDevice;
 
-	  private BanknoteInsertionSlot insertionSlot;
-	    private BanknoteValidator validator;
-	    private BanknoteStorageUnit storageUnit;
-	    private BanknoteDispenserBronze dispenser;
-	    //Banknotdispersier Gold 
-	    private BigDecimal amountDue;
-	    private Currency currency;
-	    private boolean waitingForValidation;
+import com.jjjwelectronics.OverloadedDevice;
 
-	    public PayWithBanknote(BanknoteInsertionSlot insertionSlot, BanknoteValidator validator,
-	                                 BanknoteStorageUnit storageUnit, BanknoteDispenserBronze dispenser,
-	                                 BigDecimal amountDue, Currency currency) {
-	        this.insertionSlot = insertionSlot;
-	        this.validator = validator;
-	        this.storageUnit = storageUnit;
-	        this.dispenser = dispenser;
-	        this.amountDue = amountDue;
-	        this.currency = currency;
-	        this.waitingForValidation = false;
-
-	       
-	        validator.attach(this);
-	    }
-
-	    public void pay(Banknote banknote) throws Exception {
-	        if (!banknote.getCurrency().equals(currency)) {
-	            throw new IllegalArgumentException("Invalid currency.");
-	        }
-
-	        insertionSlot.receive(banknote);
-	        validator.receive(banknote);
-	        waitingForValidation = true;
-	    }
-	    
-	    // pay (10 dollars)
-	    // goodBanknote(
-
-	    @Override
-	    public void goodBanknote(BanknoteValidator validator, Currency currency, BigDecimal denomination) {
-	        if (!waitingForValidation) return;
-	        waitingForValidation = false;
-
-	        updateTransactionTotal(denomination);
-	        try {
-	            dispenseChange();
-	        } catch (Exception e) {
-	            e.printStackTrace(); // create a loop here for not sufficient change 
-	        }
-	    }
-
-	    @Override
-	    public void badBanknote(BanknoteValidator validator) {
-	        if (!waitingForValidation) return;
-	        waitingForValidation = false;
-
-	    }
-
-	    private void updateTransactionTotal(BigDecimal banknoteValue) {
-	        amountDue = amountDue.subtract(banknoteValue);
-	    }
-
-	    
-
-	        
-
-	    private void dispenseChange() throws Exception {
-	        if (amountDue.compareTo(BigDecimal.ZERO) >= 0) {
-	            return; // No change required
-	        }
-
-	        BigDecimal changeAmount = amountDue.negate();
-
-	        // Simplified logic to dispense change
-	        while (changeAmount.compareTo(BigDecimal.ZERO) > 0) {
-	            Banknote banknoteToDispense = selectBanknoteForChange(changeAmount);
-	            if (banknoteToDispense != null) {
-	                dispenser.emit(); // Emit the banknote
-	                changeAmount = changeAmount.subtract(banknoteToDispense.getDenomination());
-	            } else {
-	                // No suitable banknote available for dispensing
-	                throw new Exception("Unable to dispense the required change.");
-	            }
-	        }
-	    }
-
-	        private Banknote selectBanknoteForChange(BigDecimal changeAmount) {
-	            
-	            if (changeAmount.compareTo(new BigDecimal("20")) >= 0) {
-	                return new Banknote(currency, new BigDecimal("20"));
-	            } else if (changeAmount.compareTo(new BigDecimal("10")) >= 0) {
-	                return new Banknote(currency, new BigDecimal("10"));
-	            } // ... add other denominations as needed
-	            else {
-	                return null; // No suitable denomination found
-	            }
-	        
-
-	    }
+import com.jjjwelectronics.printer.*;
 
 
+public class PayWithBanknoteUpdated {
 
-	//amount that the cuomer gave 
-	 
-	// amount of cahnge due 
-
-		@Override
-		public void enabled(IComponent<? extends IComponentObserver> component) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void disabled(IComponent<? extends IComponentObserver> component) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void turnedOn(IComponent<? extends IComponentObserver> component) {
-			// TODO Auto-generated method stub
-			
-		}
-
-		@Override
-		public void turnedOff(IComponent<? extends IComponentObserver> component) {
-			// TODO Auto-generated method stub
-			
-		}
+	private BanknoteInsertionSlot insertionSlot;
+	private BanknoteValidator banknoteValidator;
+	public Currency currency;
+	//public BigDecimal amountInserted;
+	public BigDecimal changeRemaining;
+	// to get the total cost of all banknotes that are inserted
+	private BigDecimal valueOfAllBanknotes = new BigDecimal("0");
+	public BigDecimal totalCost;
+	//not sure
+	private SelfCheckoutStationBronze checkoutSystem = null;
+	//the type of bills that are accepted
+	private BigDecimal[] denominations = { new BigDecimal(500), new BigDecimal(200), new BigDecimal(100),
+			new BigDecimal(50), new BigDecimal(20), new BigDecimal(10), new BigDecimal(5) };
 	
+	private ReceiptPrinterGold gold;
 
-	
+	//remove this once part of the paymentHandler class
+	public PayWithBanknoteUpdated(BigDecimal totalAmount) {
 
-
+		this.totalCost = totalAmount;
+		// create a new instance of insertionSlot
+		insertionSlot = new BanknoteInsertionSlot();
+		currency = Currency.getInstance("CAD");
+		// set up the validator so that it's cad and the bills are from 5 to 500
+		// The issue is how do we check if it's valid!!
+		banknoteValidator = new BanknoteValidator(currency, denominations);
+		
+	}
+	/**
+	 * Update the totalCost if not enough money is giving
+	 * as in constructor we only create instance of PayWithBanknote once 
+	 * If adding to paymentHandler can remove later
+	 * @param totalAmount
+	 */
+	public void setTotalCost(BigDecimal totalAmount) {
+		this.totalCost = totalAmount;
 	}
 
+	/**
+	 * will be used to help with Signaling to the Customer the updated amount due
+	 * after the insertion of each coin.
+	 *
+	 * @return money left to pay
+	 */
+	public BigDecimal getCostRemaining() {
+		return this.changeRemaining;
+	}
+
+	/**
+	 * Processes payment using coins inserted by the customer.
+	 *
+	 * @param coinsList List of coins inserted by the customer.
+	 * @return true if payment is successful, false otherwise.
+	 * @throws DisabledException        If the coin slot is disabled.
+	 * @throws CashOverloadException    If the cash storage is overloaded.
+	 * @throws NoCashAvailableException If no cash is available for dispensing
+	 *                                  change.
+	 * @throws OutOfInkException
+	 * @throws OutOfPaperException
+	 * @throws OverloadedDevice
+	 * @throws EmptyDevice
+	 */
+	public boolean processPaymentWithBanknotes(ArrayList<Banknote> BanknotesList)
+			throws DisabledException, CashOverloadException, NoCashAvailableException, EmptyDevice, OverloadedDevice {
+		
+		// first check if parameter is null or not
+		if (BanknotesList == null) {
+			throw new NullPointerException("Banknotes cannot be null.");
+		}
+		//moved instances of BanknoteValidator and BanknoteInsertion
+		
+		
+		for (Banknote banknote : BanknotesList) { // Calculate the total value of coins inserted.
+			/*
+			 * banknote is passed to the sink and a "banknoteInserted" event is announced;
+			 * otherwise, a "banknoteEjected" event is announced
+			 */
+				
+			insertionSlot.receive(banknote);
+			
+			// have to check if banknote is valid now using Validator:
+			banknoteValidator.receive(banknote);
+			
+			//After checking if valid have to get total cost
+			//getDenomination() returns the value of the note
+			valueOfAllBanknotes = valueOfAllBanknotes.add(banknote.getDenomination());
+		}
+		
+		//checks if the amount inserted is less then cost
+		if(valueOfAllBanknotes.compareTo(totalCost)< 0 ) {
+			return false;
+		}
+		//if value is equal or greater then cost
+		// have to calculate the change value
+		this.changeRemaining = valueOfAllBanknotes.subtract(this.totalCost);
+		if(changeRemaining.compareTo(new BigDecimal(0)) > 0) {
+			return dispenseAccurateChange(changeRemaining);
+		}
+		
+		return true;
+	}
+
+	//Are we using the old one for this?
+	//do we need to change the old one slightly
+	private boolean dispenseAccurateChange(BigDecimal changeRemaining) {
+		
+		return false;
+	}
+
+}
 
 // 1. Customer: Inserts a banknote in the System.
 // 2. System: Reduces the remaining amount due by the value of the inserted banknote.
